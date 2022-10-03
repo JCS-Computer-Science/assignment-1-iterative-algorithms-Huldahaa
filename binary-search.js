418ebfe5808b41c534a3069d358fdbbfa9c84a6d
Huldah Abi-Abiola <36006503@edu.cbe.ab.ca>
2022-10-03 11:06:51 -0600

    binary search, huldah

--- binary-search.js
+++ binary-search.js
@@ -1,14 +1,33 @@
 function binarySearch(array, searchTerm) {
-	/*
-	 ** Check the middle element, if it's higher than the searchTerm
-	 ** then check the element in between the start and the middle, otherwise
-	 ** check the element between the middle and the end
+   var min = 0;
+   var max = array.length -1;
+   var guess;
+   while(max => min) {
+   guess = Math.floor((max + min)/2);
+   if(array[guess] === searchTerm) {
+       return guess;
+   } else if (array[guess] < searchTerm) {
+       min = guess + 1;
+   } else {
+       max = guess - 1;
+   }
+   }
+   return -1;
+		/*
+     ** Check the middle element, if it's higher than the searchTerm
+     ** then check the element in between the start and the middle, otherwise
+     ** check the element between the middle and the end
+ 
+     ** repeat the above process for the subset of the array
+     ** keep repeating with smaller and smaller subsets until you find the searchTerm
+     */
+ 
+	 return null;
+};
 
-	 ** repeat the above process for the subset of the array
-	 ** keep repeating with smaller and smaller subsets until you find the searchTerm
-	 */
+var numbers = [2, 1, 8, 77, 34, 92, 7, 3, 88, 6, 0, 15];
+var result = binarySearch(numbers, 92);
 
-	return null;
-}
+
 
 module.exports = binarySearch;
--- linear-search.js
+++ linear-search.js
@@ -1,12 +1,16 @@
 function linearSearch(array, searchTerm) {
-	/*
+	
+	
+
+	return null;
+}
+
+module.exports = linearSearch;
+
+/*
 	 ** Check each element from start to finish
 	 ** until you find an element that matches the search term
 	 **
 	 ** Return the index of the found element
 	 */
 
-	return null;
-}
-
-module.exports = linearSearch;
