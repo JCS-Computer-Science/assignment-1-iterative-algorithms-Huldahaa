01c3969a71e1e791de7b9dc2b4d7cc5dce62ad7d
Huldah Abi-Abiola <36006503@edu.cbe.ab.ca>
2022-10-11 10:02:08 -0600

    Huldah linear search

--- linear-search.js
+++ linear-search.js
@@ -1,11 +1,19 @@
 function linearSearch(array, searchTerm) {
 	
+	for(let i = 0; i < array.length; i++) {
+		if (array[i] == searchTerm)
+		return i;
+		
+	}
 	
-
-	return null;
+	
 }
 
 module.exports = linearSearch;
+// let numbers = [3, 7, 2, 9, 1, 5 ];
+// let searchTerm = 9;
+// let array = array.length;
+
 
 /*
 	 ** Check each element from start to finish
